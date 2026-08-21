abff6808fe6f5a5860213aafa8b61553aa4c1e54
pahasara-ridmaka <pahasara.ridmaka95@gmail.com>
2026-08-22 02:11:11 +0530

    Add forms for Company and Application models with appropriate widgets

---
+++ applications/forms.py
@@ -0,0 +1,66 @@
+from django import forms
+
+from .models import Application, Company
+
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = ["name", "website", "location"]
+        widgets = {
+            "name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "e.g. Google"}
+            ),
+            "website": forms.URLInput(
+                attrs={"class": "form-control", "palceholder": "https://..."}
+            ),
+            "location": forms.TextInput(
+                attrs={"class": "forma-control", "placeholder": "e.g. Colombo / Remote"}
+            ),
+        }
+
+
+class ApplicationForm(forms.ModelForm):
+    company_name = forms.CharField(
+        max_length=200,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "e.g. Google"}
+        ),
+    )
+
+    class Meta:
+        model = Application
+        fields = [
+            "job_title",
+            "job_url",
+            "job_description",
+            "status",
+            "platform",
+            "salary_estimate",
+            "resume_file",
+            "cover_letter_file",
+        ]
+
+        widgets = {
+            "job_title": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "e.g. Software Engineer"}
+            ),
+            "job_url": forms.URLInput(
+                attrs={"class": "form-control", "placeholder": "https://..."}
+            ),
+            "job_description": forms.Textarea(
+                attrs={"class": "form-control", "placeholder": "Job description..."}
+            ),
+            "status": forms.Select(attrs={"class": "form-control"}),
+            "platform": forms.Select(attrs={"class": "form-control"}),
+            "salary_estimate": forms.NumberInput(
+                attrs={"class": "form-control", "placeholder": "e.g. 100000"}
+            ),
+            "resume_file": forms.ClearableFileInput(
+                attrs={"class": "form-control-file"}
+            ),
+            "cover_letter_file": forms.ClearableFileInput(
+                attrs={"class": "form-control-file"}
+            ),
+        }
+
