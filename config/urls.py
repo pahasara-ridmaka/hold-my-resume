ebc9e87ddb3f7f8a9b6fe9d4efae2ff00773fd92
pahasara-ridmaka <pahasara.ridmaka95@gmail.com>
2026-08-22 12:08:59 +0530

    Add logout URL to urlpatterns for user session management

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
+
 from applications import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('applications.urls') )
+    path("admin/", admin.site.urls),
+    path("", include("applications.urls")),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
