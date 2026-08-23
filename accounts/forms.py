ab273d8007ec1e4c366723ba22bf5a897bd01d41
pahasara-ridmaka <pahasara.ridmaka95@gmail.com>
2026-08-23 15:35:27 +0530

    Refactor ProfileUpdateForm to remove username field and associated widgets for simplification

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+# forms.py
 from django import forms
 from django.contrib.auth import get_user_model
 
@@ -6,33 +7,7 @@ User = get_user_model()
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ["first_name", "last_name", "username", "email"]
-        widgets = {
-            "first_name": forms.TextInput(
-                attrs={
-                    "class": "w-full bg-transparent border-0 p-0 text-sm font-semibold text-black focus:ring-0 focus:outline-none placeholder:text-neutral-400",
-                    "placeholder": "Your first name",
-                }
-            ),
-            "last_name": forms.TextInput(
-                attrs={
-                    "class": "w-full bg-transparent border-0 p-0 text-sm font-semibold text-black focus:ring-0 focus:outline-none placeholder:text-neutral-400",
-                    "placeholder": "Your last name",
-                }
-            ),
-            "username": forms.TextInput(
-                attrs={
-                    "class": "w-full bg-transparent border-0 p-0 text-sm font-semibold text-black focus:ring-0 focus:outline-none placeholder:text-neutral-400",
-                    "placeholder": "username",
-                }
-            ),
-            "email": forms.EmailInput(
-                attrs={
-                    "class": "w-full bg-transparent border-0 p-0 text-sm font-semibold text-black focus:ring-0 focus:outline-none placeholder:text-neutral-400",
-                    "placeholder": "you@example.com",
-                }
-            ),
-        }
+        fields = ["first_name", "last_name", "email"]
 
     def clean_email(self):
         email = self.cleaned_data.get("email")
