1230a6cf3a00edff55676c32fb6b7bcc88f49ef4
pahasara-ridmaka <pahasara.ridmaka95@gmail.com>
2026-08-24 16:38:57 +0530

    Add home, login, and register templates with structured layout and styling

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,9 @@ from . import views
 app_name="accounts"
 
 urlpatterns = [
-    path('', views.auth_view, name='login'),
-    path('register/', views.auth_view, name='register'),
+    path('', views.home_view, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
     path('settings/', views.edit_profile, name='edit_profile'),
     path('settings/password/', views.change_password, name='change_password'),
 ]
